0298b23e480570e45ad938385100252e0b5a07ec
MikusR <mikus.rakis@gmail.com>
2023-10-13 17:24:33 +0300

    RMO-1 Rick and Morty "Video Store" (Episodes Collection)

--- main.php
+++ main.php
@@ -12,7 +12,7 @@ class Episode
     //private array $characters;
     //private string $url;
     //private string $created;
-    private ?int $ratining;
+    private ?int $rating;
 
     public function __construct(stdClass $episodeData, int $rating = null)
     {
@@ -20,18 +20,19 @@ class Episode
             if (property_exists(__CLASS__, $propertyName)) {
                 $this->$propertyName = $value;
             }
-            $this->ratining = $rating;
+            $this->rating = $rating;
         }
-    }
-
-    public function __toString(): string
-    {
-        return;
     }
 
     /**
      * @return string
      */
+    public function __toString(): string
+    {
+        return "Season {$this->getSeason()}, Episode {$this->getEpisodeNumber()}, Title: {$this->getName()}";
+    }
+
+
     /**
      * @return int
      */
@@ -40,9 +41,9 @@ class Episode
         return $this->id;
     }
 
-    public function getEpisode(): int
+    public function getEpisodeNumber(): int
     {
-        return (int)substr($this->episode, 3, 2);
+        return (int)substr($this->episode, 4, 2);
     }
 
     public function getSeason(): int
@@ -56,6 +57,22 @@ class Episode
     public function getName(): string
     {
         return $this->name;
+    }
+
+    /**
+     * @param int $rating
+     */
+    public function setRating(int $rating): void
+    {
+        $this->rating = $rating;
+    }
+
+    /**
+     * @return int|null
+     */
+    public function getRating(): ?int
+    {
+        return $this->rating;
     }
 }
 
@@ -97,6 +114,31 @@ class ListOfEpisodes
         }
     }
 
+    public function listEpisodes(string $type = 'all'): array
+    {
+        switch ($type) {
+            case 'all':
+                return $this->episodes;
+            case 'unrated':
+                $return = [];
+                foreach ($this->episodes as $episode) {
+                    if (is_null($episode->getRating())) {
+                        $return[] = $episode;
+                    }
+                }
+                return $return;
+            case'rated':
+                $return = [];
+                foreach ($this->episodes as $episode) {
+                    if ($episode->getRating()) {
+                        $return[] = $episode;
+                    }
+                }
+                return $return;
+            default:
+                return [];
+        }
+    }
 
     public function get(int $episodeID): Episode
     {
@@ -104,6 +146,68 @@ class ListOfEpisodes
     }
 }
 
-$test = new ListOfEpisodes();
-$test->get(1)->toString();
+class Application
+{
+    private ListOfEpisodes $listOfEpisodes;
+    private array $ratings = [];
+
+    public function __construct()
+    {
+//        $this->ratings = $this->loadRatings();
+        $this->listOfEpisodes = new ListOfEpisodes();
+    }
+
+    public function list(string $type = 'all'): void
+    {
+        foreach ($this->listOfEpisodes->listEpisodes($type) as $episode) {
+            if ($episode->getEpisodeNumber() === 1) {
+                echo "Season {$episode->getSeason()}\n";
+            }
+            echo "ID:({$episode->getId()}) {$episode->getEpisodeNumber()}. {$episode->getName()}.";
+            echo ($episode->getRating()) ? " Rating: {$episode->getRating()}\n" : "\n";
+        }
+    }
+
+    public function rate(): void
+    {
+        echo "Enter episode ID to rate\n";
+        $choice = (int)readline();
+        echo "Enter rating for {$this->listOfEpisodes->get($choice)}\n";
+        $rating = (int)readline();
+
+        $this->listOfEpisodes->get($choice)->setRating($rating);
+    }
+
+    public function run(): void
+    {
+        while (true) {
+            echo "1. list episodes\n";
+            echo "2. rate episodes\n";
+            echo "3. list rated episodes\n";
+            echo "4. list unrated episodes\n";
+            echo "any other key to quit\n";
+            $choice = (int)readline();
+
+            switch ($choice) {
+                case 1:
+                    $this->list();
+                    break;
+                case 2:
+                    $this->rate();
+                    break;
+                case 3:
+                    $this->list('rated');
+                    break;
+                case 4:
+                    $this->list('unrated');
+                    break;
+                default:
+                    die;
+            }
+        }
+    }
+}
+
+$app = new Application();
+$app->run();
 //file_put_contents('test.json', json_encode($test));
